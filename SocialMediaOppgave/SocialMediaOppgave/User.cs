c92c5e8800439c79666b84391ac65d2ed1e6f54b
Bethina S.M <bethina.s.moen@gmail.com>
2025-01-03 14:49:04 +0100

    class User

---
+++ SocialMediaOppgave/SocialMediaOppgave/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaOppgave
+{
+    internal class User
+    {
+        public string Name;
+        public int Birthday;
+        public string Bio;
+        public List<User> Friends { get; private set; } = new List<User>();
+    }
+}
